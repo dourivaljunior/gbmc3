b5abd4449bf0f5df2594c3b02688a34e0aac9c74
Prof. Dourival Júnior <100372396+dourivaljunior@users.noreply.github.com>
2026-05-09 16:31:35 -0300

    Update musicas.js

--- musicas.js
+++ musicas.js
@@ -15,7 +15,6 @@ const musicData = [
         backingTrack: "flores-titãs-bt.mp3",
         txt: "flores-titãs.txt"
     },
-    // Adicione mais músicas aqui seguindo o modelo
     {
         id: 3,
         nome: "Ana Júlia",
@@ -37,7 +36,105 @@ const musicData = [
         nome: "Lanterna dos Afogados",
         artista: "Paralamas do Sucesso",
         mp3: "lanterna-dos-afogados-paralamas-do-sucesso.mp3",
-        backingTrack: "lanterna-dos-afogados-paralamas-do-sucesso.mp3",
+        backingTrack: "lanterna-dos-afogados-paralamas-do-sucesso-bt.mp3",
         txt: "lanterna-dos-afogados-paralamas-do-sucesso.txt"
     },
+    {
+        id: 6,
+        nome: "Medo de Avião",
+        artista: "Belchior",
+        mp3: "medo-de-avião-belchior.mp3",
+        backingTrack: "medo-de-avião-belchior-bt.mp3",
+        txt: "medo-de-avião-belchior.txt"
+    },
+    {
+        id: 7,
+        nome: "Saia do meu Caminho",
+        artista: "Belchior",
+        mp3: "saia-do-meu-caminho-belchior.mp3",
+        backingTrack: "saia-do-meu-caminho-belchior-bt.mp3",
+        txt: "saia-do-meu-caminho-belchior.txt"
+    },
+     {
+        id: 8,
+        nome: "Meu Erro",
+        artista: "Paralamas do Sucesso",
+        mp3: "meu-erro-paralamas-do-sucesso.mp3",
+        backingTrack: "meu-erro-paralamas-do-sucesso-bt.mp3",
+        txt: "meu-erro-paralamas-do-sucesso.txt"
+    },
+     {
+        id: 9,
+        nome: "Será",
+        artista: "Legião Urbana",
+        mp3: "será-legião-urbana.mp3",
+        backingTrack: "será-legião-urbana-bt.mp3",
+        txt: "será-legião-urbana.txt"
+    },
+     {
+        id: 10,
+        nome: "Sonifera Ilha",
+        artista: "Titãs",
+        mp3: "sonifera-ilha-titãs.mp3",
+        backingTrack: "sonifera-ilha-titãs-bt.mp3",
+        txt: "sonifera-ilha-titãs.txt"
+    },
+     {
+        id: 11,
+        nome: "SOS (disco Voador)",
+        artista: "Raul Seixas",
+        mp3: "sos-raul-seixas.mp3",
+        backingTrack: "sos-raul-seixas-bt.mp3",
+        txt: "sos-raul-seixas.txt"
+    },
+    {
+        id: 12,
+        nome: "Morena Tropicana",
+        artista: "Alceu Valença",
+        mp3: "morena-tropicana-alceu-valença.mp3",
+        backingTrack: "morena-tropicana-alceu-valença-bt.mp3",
+        txt: "morena-tropicana-alceu-valença.txt"
+    },
+    {
+        id: 13,
+        nome: "Go Back",
+        artista: "Titãs",
+        mp3: "go-back-titãs.mp3",
+        backingTrack: "go-back-titãs-bt.mp3",
+        txt: "go-back-titãs.txt"
+    },
+    {
+        id: 14,
+        nome: "Tempos Modernos",
+        artista: "Lulu Santos",
+        mp3: "tempos-modernos-lulu-santos.mp3",
+        backingTrack: "tempos-modernos-lulu-santos-bt.mp3",
+        txt: "tempos-modernos-lulu-santos.txt"
+    },
+    {
+        id: 15,
+        nome: "O Sol",
+        artista: "Jota Quest",
+        mp3: "o-sol-jota-quest.mp3",
+        backingTrack: "o-sol-jota-quest-bt.mp3",
+        txt: "o-sol-jota-quest.txt"
+    },
+    {
+        id: 16,
+        nome: "Primeiros Erros",
+        artista: "Capital Inicial",
+        mp3: "primeiros-erros-capital-inicial.mp3",
+        backingTrack: "primeiros-erros-capital-inicial-bt.mp3",
+        txt: "primeiros-erros-capital-inicial.txt"
+    },
+    {
+        id: 17,
+        nome: "O Descobridor dos Sete Mares",
+        artista: "Tim Maia",
+        mp3: "o-descobridor-dos-sete-mares-tim-maia.mp3",
+        backingTrack: "o-descobridor-dos-sete-mares-tim-maia-bt.mp3",
+        txt: "o-descobridor-dos-sete-mares-tim-maia.txt"
+    },
+
+    
 ];
