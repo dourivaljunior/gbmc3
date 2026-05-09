77293869878cdf4f45ac99791aac27c418c6ae4f
Prof. Dourival Júnior <100372396+dourivaljunior@users.noreply.github.com>
2026-05-09 15:38:01 -0300

    Update musicas.js

--- musicas.js
+++ musicas.js
@@ -3,16 +3,16 @@ const musicData = [
         id: 1,
         nome: "Gita",
         artista: "Raul Seixas",
-        mp3: "gita.mp3",
-        backingTrack: "gitabt.mp3",
+        mp3: "gita-raul-seixas.mp3",
+        backingTrack: "gita-raul-seixas-bt.mp3",
         txt: "gita-raul-seixas.txt"
     },
     {
         id: 2,
         nome: "Flores",
         artista: "Titãs",
-        mp3: "flores.mp3",
-        backingTrack: "flores-bt.mp3",
+        mp3: "flores-titãs.mp3",
+        backingTrack: "flores-titãs-bt.mp3",
         txt: "flores-titãs.txt"
     },
     // Adicione mais músicas aqui seguindo o modelo
@@ -20,16 +20,16 @@ const musicData = [
         id: 3,
         nome: "Ana Júlia",
         artista: "Los Hermanos",
-        mp3: "ana_julia.mp3",
-        backingTrack: "ana_julia-bt.mp3",
+        mp3: "ana-julia-los-hermanos.mp3",
+        backingTrack: "ana-julia-los-hermanos-bt.mp3",
         txt: "ana-julia-los-hermanos.txt"
     },
     {
         id: 4,
         nome: "Tempo Perdido",
         artista: "Legião Urbana",
-        mp3: "tempo-perdido.mp3",
-        backingTrack: "tempo-perdido-bt.mp3",
+        mp3: "tempo-perdido-legião-urbana.mp3",
+        backingTrack: "tempo-perdido-legião-urbana-bt.mp3",
         txt: "tempo-perdido-legião-urbana.txt"
     },
 ];
